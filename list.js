f9afb277ad1935ed68159ee54808dc6dc944768f
eihab-dev <108094128+eihab-dev@users.noreply.github.com>
2023-04-15 23:13:58 +0000

    Create list.js

---
+++ list.js
@@ -0,0 +1,295 @@
+const list =[
+    {"index":1,"category":"السيارات",
+    "subcategorys":[
+        {"index":1.1,"subcategory":"سيارات للبيع"},
+        {"index":1.2,"subcategory":"سيارات للإيجار"},
+        {"index":1.3,"subcategory":"لوحات أرقام السيارات"},
+        {"index":1.4,"subcategory":"قطع غيار السيارات"},
+        {"index":1.5,"subcategory":"ملحقات السيارة"},
+        {"index":1.6,"subcategory":"العجلات والحافات"},
+        {"index":1.7,"subcategory":"الشاحنات والمقطورات"},
+        {"index":1.8,"subcategory":"الآلات الثقيلة"},
+        {"index":1.9,"subcategory":"القارب , جيت سكي"},
+        {"index":1.10,"subcategory":"السيارات غير المهمة"}
+    ]
+},
+
+
+    {"index":2,"category":"الدراجات النارية" ,
+    "subcategorys": [
+        {"index":2.1,"subcategory":"الدراجات النارية"},
+        {"index":2.2,"subcategory":"الخوذات والملابس"},
+        {"index":2.3,"subcategory":"الأجزاء و الملحقات"},
+        {"index":2.4,"subcategory":"أخرى"},
+    ]
+},
+
+
+    {"index": 3 , "category":"الجوالات-الأجهزة اللوحية",
+    "subcategorys":[
+        {"index":3.1,"subcategory":"الجوالات"},
+        {"index":3.2,"subcategory":"الأجهزة اللوحية"},
+        {"index":3.3,"subCategory":"weatches الذكية"},
+        {"index":3.4,"subcategory":"سماعات الرأس"},
+        {"index":3.5,"subcategory":"الأغلفة والحماية"},
+        {"index":3.6,"subcategory":"الشواحن والكابلات"},
+        {"index":3.7,"subCategory":"الجوالات - قطع غيار الكمبيوتر اللوحي"},
+        {"index":3.8,"subcategory":"الملحقات"},
+        {"index":3.9,"subCategory":"Vip Phone Numbers"},
+        {"index":3.10,"subCategory":"Mobil & Tablet Repair Services"},
+        {"index":3.11,"subcategory":"أخرى"}
+    ]
+},
+
+
+        {"index": 4 ,"category":"الفيديو ووحدات التحكم",
+          "subcategorys": [
+              {"index":4.1, "subcategory": "وحدات التحكم"},
+              {"index":4.2, "subcategory": "ألعاب الفيديو"},
+              {"index":4.3, "subcategory": "الملحقات"},
+              {"index":4.4, "subcategory": "Gaming Cards"},
+              {"index":4.5, "subcategory": "الحسابات والشخصيات"},
+              {"index":4.6, "subcategory": "شخصيات الحركة"},
+              {"index":4.7, "subcategory": "خدمات إصلاح ألعاب الفيديو"},
+              {"index":4.8, "subcategory": "أخرى"},
+            ]
+        },
+
+        {"index":5,"category":"أجهزة الكمبيوتر وأجهزة الكمبيوتر المحمولة" ,
+         "subcategory": [
+             {"index":5.1,"subcategory": "أجهزة الكمبيوتر المحمولة"},
+             {"index":5.2,"subcategory": "أجهزة الكمبيوتر"},
+             {"index":5.3,"subCategory": " كمبيوتر  ألعاب"},
+             {"index":5.4,"subcategory": "الشاشات"},
+             {"index":5.5,"subcategory": "الطابعات والملحقات"},
+             {"index":5.6,"subcategory": "المكونات"},
+             {"index":5.7,"subcategory": "محركات الأقراص والتخزين"},
+             {"index":5.8,"subcategory": "أجهزة المودم وأجهزة التوجيه"},
+             {"index":5.9,"subcategory": "الخوادم"},
+             {"index":5.10,"subcategory": "أجهزة العرض"},
+             {"index":5.11,"subcategory": "البرامج والأنظمة"},
+             {"index":5.12,"subcategory": "ملحقات الكمبيوتر"},
+             {"index":5.13,"subcategory": "خدمات إصلاح الكمبيوتر"},
+            ]
+        },
+
+        {"index":6,"category": "الإلكترونيات",
+             "subcategory": [
+                 {"index":6.1,"subCategory": "التلفزيونات والشاشات"},
+                 {"index":6.2,"subCategory": "Small Kitchen Appliances"},
+                 {"index":6.3,"subcategory": "ثلاجات ومجمدات"},
+                 {"index":6.4,"subCategory": "Washing mashines & Dryers"},
+                 {"index":6.5,"subcategory": "غسالات الصحون"},
+                 {"index":6.6,"subcategory": "الأفران"},
+                 {"index":6.7,"subcategory": "أجهزة الميكروويف"},
+                 {"index":6.8,"subCategory": "مكيفات الهواء"},
+                 {"index":6.9,"subcategory": "الكاميرات والتصوير"},
+                 {"index":6.10,"subcategory": "أجهزة الاستقبال"},
+                 {"index":6.11,"subcategory": "الصوت والفيديو"},
+                 {"index":6.12,"subcategory": "الأجهزة المنزلية"},
+                 {"index":6.13,"subCategory": "Personal Care Devices"},
+                 {"index":6.14,"subCategory": "سخانات"},
+                 {"index":6.15,"subCategory": "Water & Solar Heater"},
+                 {"index":6.16,"subCategory": "مكنسة كهربائية"},
+                 {"index":6.17,"subCategory": "Water Coolers and Filters"},
+                 {"index":6.18,"subCategory": "Security and Surveillance"},
+                 {"index":6.19,"subcategory": "الهواتف والفاكسات"},
+                 {"index":6.20,"subcategory": "الملحقات والأجزاء"},
+                 {"index":6.21,"subcategory": "خدمات إصلاح الأجهزة"},
+                ]
+            },
+            {"index": 7 , "category": "عقارات للبيع" ,
+            "subcategory": [
+                {"index":7.1,"subcategory": "شقق للبيع"},
+                {"index":7.2,"subCategory": "فيلا - قصر للبيع"},
+                {"index":7.3,"subCategory": "تاون هاوس للبيع"},
+                {"index":7.4,"subcategory": "أراضي للبيع"},
+                {"index":7.5,"subcategory": "تجاري للبيع"},
+                {"index":7.6,"subCategory": "مزارع وشاليهات للبيع"},
+                {"index":7.7,"subCategory": "عقارات أجنبية للبيع"},
+            ]
+        },
+
+        {"index":8,"category": "عقارات للإيجار",
+         "subcategory": [
+             {"index":8.1,"subcategory": "شقة للإيجار"},
+             {"index":8.2,"subcategory": "Commmercial for Rant"},
+             {"index":8.3,"subcategory": "الشقق والأجنحة الفندقية"},
+             {"index":8.4,"subcategory": "غرف مشتركة للإيجار"},
+             {"index":8.5,"subcategory": "فيلا - قصر الخرف"},
+             {"index":8.6,"subcategory": "تاون هاوس للإيجار"},
+             {"index":8.7,"subcategory": "مبنى كامل للإيجار"},
+             {"index":8.8,"subcategory": "أراضي للإيجار"},
+             {"index":8.9,"subCategory": "مزارع وشاليهات للإيجار"},
+             {"index":8.10,"subcategory": "عقارات أجنبية للإيجار"},
+            ]
+        },
+
+
+
+        {"index": 9,"category": "المنزل والحديقة",
+         "subcategory": [
+             {"index":9.1,"subcategory": "أثاث غرفة المعيشة"},
+             {"index":9.2,"subcategory": "أثاث غرف النوم"},
+             {"index":9.3,"subCategory": "أثاث مكتبي"},
+             {"index":9.4,"subCategory": "Out Door Furniture"},
+             {"index":9.5,"subcategory": "أثاث الأطفال"},
+             {"index":9.6,"subCategory": "Decoration & Accssories"},
+             {"index":9.7,"subcategory": "فرشات ومنسوجات"},
+             {"index":9.8,"subcategory": "الحدائق - النباتات"},
+             {"index":9.9,"subcategory": "المطابخ"},
+             {"index":9.10,"subcategory": "أدوات المطبخ"},
+             {"index":9.11,"subcategory": "الحمامات"},
+             {"index":9.12,"subcategory": "Taile and Flooring"},
+             {"index":9.13,"subcategory": "الأبواب - النوافذ - الألومنيوم"},
+             {"index":9.14,"subcategory": "الإضاءة"},
+             {"index":9.15,"subcategory": "السجاد"},
+             {"index":9.16,"subcategory": "الستائر"},
+             {"index":9.17,"subcategory": "خدمات التنجيد"},
+             {"index":9.18,"subcategory": "خدمات الحدائق"},
+             {"index":9.19,"subcategory": "أخرى"},
+         ]
+     } ,
+
+     {"index":10,"category": "أزياء نسائية",
+         "subcategory": [
+             {"index":10.1,"subcategory": "الملابس"},
+             {"index":10.2,"subCategory": "أحذية نسائية"},
+             {"index":10.3,"subcategory": "الساعات"},
+             {"index":10.4,"subcategory": "الإكسسوارات - المجوهرات"},
+             {"index":10.5,"subcategory": "الحقائب"},
+             {"index":10.6,"subcategory": "العطور - البخور"},
+             {"index":10.7,"subCategory": "Beauty Cosmatics"},
+             {"index":10.8,"subCategory": "أخرى - أزياء نسائية"},
+
+         ]
+     },
+
+
+
+     {"index":11,"category": "أزياء الرجال",
+         "subcategory": [
+             {"index": 11.1,"subCategory": " ملابس رجالية"},
+             {"index":11.2,"subcategory": "أحذية رجالية"},
+             {"index":11.3,"subCategory": "إكسسوارات الرجال"},
+             {"index":11.4,"subCategory": "منتجات العناية الشخصية"},
+             {"index":11.5,"subCategory": "العطور - البخور للرجال"},
+             {"index":11.6,"subcategory": "أخرى - أزياء الرجال"},
+         ]
+     } ,
+
+     {"index": 12 , "category": "الأطفال الرُضَّع" ,
+         "subcategory": [
+             {"index":12.1,"subcategory":"أثاث الأطفال"},
+             {"index":12.2,"subcategory":"منتجات الأطفال"},
+             {"index":12.3,"subcategory":"ملابس الأطفال"},
+             {"index":12.4,"subcategory":"الألعاب"},
+             {"index":12.5,"subcategory":"أخرى"},
+         ]
+     } ,
+     {"index":13,"category": "الطعام والمكملات الغذائية"},
+
+     {"index":14 ,"category":"التعليم والتدريب",
+         "subcategory": [
+             {"index":14.1,"subcategory":"الدورات التدريبية"},
+             {"index":14.2,"subcategory":"التدريس"},
+             {"index":14.3,"subcategory":"أخرى"},
+         ]
+     } ,
+
+     {"index": 15,"category": "الخدمات",
+         "subcategory": [
+             {"index":15.1,"subcategory": "إصلاح المنزل"},
+             {"index":15.2,"subcategory": "Movers - Pacers"} ,
+             {"index":15.3,"subcategory": "الخدمات الكهربائية"},
+             {"index":15.4,"subcategory": "السفر - السياحة"},
+             {"index":15.5,"subcategory": "خدمات إصلاح ألعاب الفيديو"},
+             {"index":15.6,"subcategory": "الخدمات الطبية"},
+             {"index":15.7,"subcategory": "خدمات الأحداث"} ,
+             {"index":15.8,"subcategory": "خدمات إصلاح الأجهزة المحمولة والأجهزة اللوحية"},
+             {"index":15.9,"subcategory": "صيانة وإصلاح السيارات"},
+             {"index":15.10,"subcategory": "خدمات السحب"},
+             {"index":15.11,"subcategory": "خدمات تنظيف السيارات"} ,
+             {"index":15.12,"subcategory": "خدمات إصلاح الكمبيوتر"},
+             {"index":15.13,"subcategory": "خدمات التنظيف"},
+             {"index":15.14,"subcategory": "الخدمات القانونية"},
+             {"index":15.15,"subcategory": "خدمات النقل والتوصيل"} ,
+             {"index":15.16,"subcategory": "خدمات الطلاء"},
+             {"index":15.17,"subcategory": "الحدادة وخدمات اللحام"},
+             {"index":15.18,"subCategory": "Appliance Repair Services"},
+             {"index":15.19,"subcategory": "خدمات التنجيد"},
+             {"index":15.20,"subCategory": "Tiles & Flooring Services"},
+             {"index":15.21, "subcategory": "النجارة وصيانة الأثاث"},
+             {"index":15.22, "subcategory": "تثبيت المطبخ"},
+             {"index":15.23, "subcategory": "خدمات الحدائق"},
+             {"index":15.24, "subcategory": "الأبواب - خدمات Windows"},
+             {"index":15.25, "subcategory": "خدمات التصميم الداخلي"},
+             {"index":15.26, "subcategory": "خدمات الإصلاح العامة"},
+             {"index":15.27, "subcategory": "التسويق والإعلان"},
+             {"index":15.28, "subcategory": "خدمات التصميم والطباعة"},
+             {"index":15.29, "subcategory": "المحاسبة والمالية"},
+             {"index":15.30, "subcategory": "خدمات التجميل"},
+             {"index":15.31, "subcategory": "خدمات الرعاية المنزلية"},
+             {"index":15.32, "subcategory": "خدمات الأعمال"},
+             {"index":15.33, "subcategory": "خدمات أخرى"},
+         ]
+     } ,
+
+     {"index":16,"category": "Jops",
+         "subcategory": [
+             {"index":16.1,"subcategory": "الباحثون عن عمل"},
+             {"index":16.2,"subcategory": "الوظائف الشاغرة"},
+         ]
+     } ,
+
+     {"index":17,"category": "الحيوانات الأليفة",
+         "subcategory": [
+             {"index":17.1,"subcategory":"الحيوانات الأليفة"},
+             {"index":17.2,"subcategory":"طعام الحيوانات الأليفة"},
+             {"index":17.3,"subcategory":"مستلزمات الحيوانات الأليفة"},
+             {"index":17.4,"subcategory":"العناية بالحيوانات الأليفة"},
+             {"index":17.5,"subcategory":"أخرى"},
+         ]
+     } ,
+
+     {"index":18,"category": "الكتب والهوايات",
+         "subcategory": [
+             {"index":18.1,"subcategory": "الكتب"},
+             {"index":18.2,"subcategory": "الآلات الموسيقية"},
+             {"index":18.3,"subcategory": "أخرى"},
+         ]
+     },
+
+     {"index":19,"category": "الرياضة واللياقة البدنية",
+         "subcategory": [
+             {"index":19.1,"subcategory":"المعدات الرياضية"},
+             {"index":19.2,"subcategory":"رفع الأثقال"},
+             {"index":19.3,"subcategory":"المقاومة والطاقة"},
+             {"index":19.4,"subcategory":"دراجات"},
+             {"index":19.5,"subcategory":"دراجات كهربائية"},
+             {"index":19.6,"subcategory":"البلياردو والسنوكر"},
+             {"index":19.7,"subcategory":"Baby Foot & Air Hockey"},
+             {"index":19.8,"subcategory":"رياضات المضرب"},
+             {"index":19.9,"subcategory":"رياضات الكرة"},
+             {"index":19.10,"subcategory": "التخييم"},
+             {"index":19.11,"subcategory": "Skate and Scootrs"} ,
+             {"index":19.12,"subcategory": "الملحقات الرياضية"},
+             {"index":19.13,"subcategory": "أخرى"},
+         ]
+     } ,
+
+     {"index":20,"category": "الأعمال - المعدات",
+         "subcategory": [
+             {"index":20.1,"subcategory": "معدات المطاعم"},
+             {"index":20.2,"subcategory": "مستلزمات المتاجر"},
+             {"index":20.3,"subcategory": "المعدات الطبية"},
+             {"index":20.4,"subCategory": "لوازم ومواد البناء"},
+             {"index":20.5,"subcategory": "الأدوات اليدوية"},
+             {"index":20.6,"subcategory": "المشاريع الاستثمارية"},
+             {"index":20.7,"subcategory": "معدات المصانع"},
+             {"index":20.8,"subCategory": "مستلزمات ومعدات التنظيف"},
+             {"index":20.9,"subcategory": "مستلزمات ومعدات الخياطة"},
+             {"index":20.10,"subcategory": "أخرى"},
+         ]
+     }
+]
