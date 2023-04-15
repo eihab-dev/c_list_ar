f1290c4a2d51b158553955765d577258e7e9d1c2
eihab-dev <108094128+eihab-dev@users.noreply.github.com>
2023-04-15 23:15:30 +0000

    Create english_list.js

---
+++ english_list.js
@@ -0,0 +1,294 @@
+const list =[
+    {"index":1,"category":"Cars",
+        "subcategorys":[
+            {"index":1.1,"subCategory":"Cars for Sale"},
+            {"index":1.2,"subCategory":"Rantal Car"},
+            {"index":1.3,"subCategory":"Car Number Plates"},
+            {"index":1.4,"subCategory":"Car Spare Parts"},
+            {"index":1.5,"subCategory":"Car Accessories"},
+            {"index":1.6,"subCategory":"Wheels and Rims"},
+            {"index":1.7,"subCategory":"Trucks & Trailers"},
+            {"index":1.8,"subCategory":"Heavy Machinery"},
+            {"index":1.9,"subCategory":"Boat,Yscht,Jet Ski"},
+            {"index":1.10,"subCategory":"Junk Cars"},
+        ]
+    },
+    
+    {"index":2,"category":"Motorcycles",
+        "subcategorys":[
+            {"index":2.1,"subCategory":"Motorcyles"},
+            {"index":2.2,"subCategory":"Helmets and Clothes"},
+            {"index":2.3,"subCategory":"Parts and Accessories"},
+            {"index":2.4,"subCategory":"Others"},
+
+    ]
+},
+    {"index":3,"category":"Mobil-Tablet",
+         "subcategorys":[
+             {"index":3.1,"subCategory":"Mobiles"},
+             {"index":3.2,"subCategory":"Tablets"},
+             {"index":3.3,"subCategory":"Smart weatches"},
+             {"index":3.4,"subCategory":"Headsets"},
+             {"index":3.5,"subCategory":"Covers and Protectors"},
+             {"index":3.6,"subCategory":"Chargers and Cables"},
+             {"index":3.7,"subCategory":"Mobail - Tablet spare parts"},
+             {"index":3.8,"subCategory":"Accessories"},
+             {"index":3.9,"subCategory":"Vip Phone Numbers"},
+             {"index":3.10,"subCategory":"Mobil & Tablet Repair Services"},
+             {"index":3.11,"subCategory":"Others"},
+    ]
+},
+
+    {"index":4,"category":"Video and Consoles",
+         "subcategorys":[
+             {"index":4.1,"subCategory":"Consoles"},
+             {"index":4.2,"subCategory":"Video Games"},
+             {"index":4.3,"subCategory":"Accessories"},
+             {"index":4.4,"subCategory":"Gaming Cards"},
+             {"index":4.5,"subCategory":"Accounts and Characters"},
+             {"index":4.6,"subCategory":"Action Figures"},
+             {"index":4.7,"subCategory":"Video Games Repair Services"},
+             {"index":4.8,"subCategory":"Others"},
+
+
+]},
+
+    {"index":5,"category":"Computers and Laptops",
+        "subcategorys":[
+            {"index":5.1,"subCategory":"Laptops"},
+            {"index":5.2,"subCategory":"Computers"},
+            {"index":5.3,"subCategory":"Gaming Pc"},
+            {"index":5.4,"subCategory":"Monitors"},
+            {"index":5.5,"subCategory":"Printers and Accessories"},
+            {"index":5.6,"subCategory":"Components"},
+            {"index":5.7,"subCategory":"Drives and Storage"},
+            {"index":5.8,"subCategory":"Modems and Routers"},
+            {"index":5.9,"subCategory":"Servers"},
+            {"index":5.10,"subCategory":"Projectors"},
+            {"index":5.11,"subCategory":"Softwares and Systems"},
+            {"index":5.12,"subCategory":"Computer Accessories"},
+            {"index":5.13,"subCategory":"Computer Repair Services "},
+    ]
+},
+
+        {"index":6,"category":"Electronics",
+            "subcategorys":[
+                {"index":6.1,"subCategory":"Tvs & Screens"},
+                {"index":6.2,"subCategory":"Small Kitchen Appliances"},
+                {"index":6.3,"subCategory":"Refrigerators & Freezers"},
+                {"index":6.4,"subCategory":"Washing mashines & Dryers"},
+                {"index":6.5,"subCategory":"Dishwashers"},
+                {"index":6.6,"subCategory":"Ovens"},
+                {"index":6.7,"subCategory":"Microwaves"},
+                {"index":6.8,"subCategory":"Air Conditioners"},
+                {"index":6.9,"subCategory":"Cameras and Photography"},
+                {"index":6.10,"subCategory":"Receivers"},
+                {"index":6.11,"subCategory":"Audio and Video"},
+                {"index":6.12,"subCategory":"Home Appliances"},
+                {"index":6.13,"subCategory":"Personal Care Devices"},
+                {"index":6.14,"subCategory":"Heaters"},
+                {"index":6.15,"subCategory":"Water & Solar Heater"},
+                {"index":6.16,"subCategory":"Vacuum Cleaner"},
+                {"index":6.17,"subCategory":"Water Coolers and Filters"},
+                {"index":6.18,"subCategory":"Security and Surveillance"},
+                {"index":6.19,"subCategory":"Phones & Fax"},
+                {"index":6.20,"subCategory":"Accessories & Parts"},
+                {"index":6.21,"subCategory":"Appliances Repair Services"},
+            ]
+        },
+
+    {"index":7,"category":"Realstate for Sale",
+        "subcategorys":[
+            {"index":7.1,"subCategory":"Apartments for Sale"},
+            {"index":7.2,"subCategory":"Villa - Palace for Sale"},
+            {"index":7.3,"subCategory":"Townhouses for Sale"},
+            {"index":7.4,"subCategory":"Lands for Sale"},
+            {"index":7.5,"subCategory":"Commercial for Sale"},
+            {"index":7.6,"subCategory":"Farms & Chalets for Sale"},
+            {"index":7.7,"subCategory":"Foreign Real Estate for Sale"},
+    ]
+},
+
+    {"index":8,"category":"Realstate for Rant",
+        "subcategorys":[
+            {"index":8.1,"subCategory":"Apartment for Rant"},
+            {"index":8.2,"subCategory":"Commmercial for Rant"},
+            {"index":8.3,"subCategory":"Hotel Apartment and Suites"},
+            {"index":8.4,"subCategory":"Shared Rooms for Rent"},
+            {"index":8.5,"subCategory":"Villa - Palace for Rant"},
+            {"index":8.6,"subCategory":"Townhouses for Rent"},
+            {"index":8.7,"subCategory":"Whole Building for Rent"},
+            {"index":8.8,"subCategory":"Lands for Rent"},
+            {"index":8.9,"subCategory":"Farms & Chalets for Rent"},
+            {"index":8.10,"subCategory":"Foreign Real Estate for Rent"},
+        ]
+    },
+
+    {"index":9,"category":"Home and Garden",
+        "subcategorys":[
+            {"index":9.1,"subCategory":"Living Room Furniture"},
+            {"index":9.2,"subCategory":"Bedroom Furniture"},
+            {"index":9.3,"subCategory":"Office Furniture"},
+            {"index":9.4,"subCategory":"Out Door Furniture"},
+            {"index":9.5,"subCategory":"Kids Furniture"},
+            {"index":9.6,"subCategory":"Decoration & Accssories"},
+            {"index":9.7,"subCategory":"Bed Matresses & Textile"},
+            {"index":9.8,"subCategory":"Garden - Plants"},
+            {"index":9.9,"subCategory":"Kitchens"},
+            {"index":9.10,"subCategory":"Kitchen Tools"},
+            {"index":9.11,"subCategory":"Bathrooms"},
+            {"index":9.12,"subCategory":"Taile and Flooring"},
+            {"index":9.13,"subCategory":"Doors - Windows - Aluminium"},
+            {"index":9.14,"subCategory":"Lighting"},
+            {"index":9.15,"subCategory":"Carpets"},
+            {"index":9.16,"subCategory":"Curtains"},
+            {"index":9.17,"subCategory":"Upholstery Services"},
+            {"index":9.18,"subCategory":"Gardening Services"},
+            {"index":9.19,"subCategory":"Other"},
+        ]
+    },
+
+    {"index":10,"category":"Womens Fashion",
+        "subcategorys":[
+            {"index":10.1,"subCategory":"Clothes"},
+            {"index":10.2,"subCategory":"Women Shoes"},
+            {"index":10.3,"subCategory":"Watches"},
+            {"index":10.4,"subCategory":"Accessories - Jewelry"},
+            {"index":10.5,"subCategory":"Bags"},
+            {"index":10.6,"subCategory":"Perfumes - Incense"},
+            {"index":10.7,"subCategory":"Beauty Cosmatics"},
+            {"index":10.8,"subCategory":"Others - Womens Fashion"},
+
+        ]
+    },
+
+    {"index":11,"category":"Mans Fashion",
+        "subcategorys":[
+            {"index":11.1,"subCategory":"Manswear"},
+            {"index":11.2,"subCategory":"Man's Shoes"},
+            {"index":11.3,"subCategory":"Men's Accssories"},
+            {"index":11.4,"subCategory":"Personal Care Products"},
+            {"index":11.5,"subCategory":"Perfume - Incense for Man"},
+            {"index":11.6,"subCategory":"Others - Man's Fashion"},
+        ]
+    },
+
+    {"index":12,"category":"Baby Kids",
+        "subcategorys":[
+            {"index":12.1,"subCategory":"Kids Furniture"},
+            {"index":12.2,"subCategory":"Baby Products"},
+            {"index":12.3,"subCategory":"Kids Clothing"},
+            {"index":12.4,"subCategory":"Toys"},
+            {"index":12.5,"subCategory":"Others"},
+        ]
+    },
+// NO SUBCAEGORY FOR NOW ADD IT LATAR
+    {"index":13,"category":"Food & Supplements"},
+
+    {"index":14,"category":"Education and Training",
+        "subcategorys":[
+            {"index":14.1,"subCategory":"Training Courses"},
+            {"index":14.2,"subCategory":"Tutoring"},
+            {"index":14.3,"subCategory":"Others"},
+        ]
+    },
+
+    {"index":15,"category":"Services",
+        "subcategorys":[
+            {"index":15.1,"subCategory":"Home Repair"},
+            {"index":15.2,"subCategory":"Movers - Pacers"},
+            {"index":15.3,"subCategory":"Electrical servicws"},
+            {"index":15.4,"subCategory":"Travel - Tourism"},
+            {"index":15.5,"subCategory":"Video Games Repair Services"},
+            {"index":15.6,"subCategory":"Medical Services"},
+            {"index":15.7,"subCategory":"Events Services"},
+            {"index":15.8,"subCategory":"Mobile & Tablet Repair Services"},
+            {"index":15.9,"subCategory":"Car Maintenance and Repair"},
+            {"index":15.10,"subCategory":"Towing Services"},
+            {"index":15.11,"subCategory":"Car Cleaning Services"},
+            {"index":15.12,"subCategory":"Computer Repair Services"},
+            {"index":15.13,"subCategory":"Cleaning Services"},
+            {"index":15.14,"subCategory":"Legal Services"},
+            {"index":15.15,"subCategory":"Transportation & Delivery Services"},
+            {"index":15.16,"subCategory":"Painting Services"},
+            {"index":15.17,"subCategory":"Blacksmithing & Welding Services"},
+            {"index":15.18,"subCategory":"Appliance Repair Services"},
+            {"index":15.19,"subCategory":"Upholstery Services"},
+            {"index":15.20,"subCategory":"Tiles & Flooring Services"},
+            {"index":15.21,"subCategory":"Carpentry and Furniture Maintenance"},
+            {"index":15.22,"subCategory":"Kitchen Installation"},
+            {"index":15.23,"subCategory":"Gardening Services"},
+            {"index":15.24,"subCategory":"Doors - Windows Services"},
+            {"index":15.25,"subCategory":"Interior Design Services"},
+            {"index":15.26,"subCategory":"General Repair Services"},
+            {"index":15.27,"subCategory":"Marketing and Advertising"},
+            {"index":15.28,"subCategory":"Design & Printing Services"},
+            {"index":15.29,"subCategory":"Accounting and Finance"},
+            {"index":15.30,"subCategory":"Beauty Services"},
+            {"index":15.31,"subCategory":"Home Care Services"},
+            {"index":15.32,"subCategory":"Business Services"},
+            {"index":15.33,"subCategory":"Other Services"},
+        ]
+    },
+
+    {"index":16,"category":"Jops",
+        "subcategorys":[
+            {"index":16.1,"subCategory":"Job Seekers"},
+            {"index":16.2,"subCategory":"Job Vacancies"},
+        ]
+    },
+
+    {"index":17,"category":"Pets",
+        "subcategorys":[
+            {"index":17.1,"subCategory":"Pets"},
+            {"index":17.2,"subCategory":"Pets Food"},
+            {"index":17.3,"subCategory":"Pets Accessories"},
+            {"index":17.4,"subCategory":"Pets Grooming"},
+            {"index":17.5,"subCategory":"Others"},
+        ]
+    },
+
+    {"index":18,"category":"Books and Hoppies",
+        "subcategorys":[
+            {"index":18.1,"subCategory":"Books"},
+            {"index":18.2,"subCategory":"Musical Instruments"},
+            {"index":18.3,"subCategory":"Others"},
+        ]
+    },
+
+    {"index":19,"category":"Sports and Fitness",
+        "subcategorys":[
+            {"index":19.1,"subCategory":"Sport Equipments"},
+            {"index":19.2,"subCategory":"Weight Lifting"},
+            {"index":19.3,"subCategory":"Resistance & Power"},
+            {"index":19.4,"subCategory":"Bicyclee"},
+            {"index":19.5,"subCategory":"Electric Bicycles"},
+            {"index":19.6,"subCategory":"Billiard & Snooker"},
+            {"index":19.7,"subCategory":"Baby Foot & Air Hockey"},
+            {"index":19.8,"subCategory":"Racket Sports"},
+            {"index":19.9,"subCategory":"Ball Sports"},
+            {"index":19.10,"subCategory":"Camping"},
+            {"index":19.11,"subCategory":"Skate and Scootrs"},
+            {"index":19.12,"subCategory":"Sport Accessories"},
+            {"index":19.13,"subCategory":"Others"},
+        ]
+    },
+
+    {"index":20,"category":"Business - Equipments",
+        "subcategorys":[
+            {"index":20.1,"subCategory":"Restaurants Equipments"},
+            {"index":20.2,"subCategory":"Shops Supplies"},
+            {"index":20.3,"subCategory":"Medical Equipments"},
+            {"index":20.4,"subCategory":"Building Supplies & Materials"},
+            {"index":20.5,"subCategory":"Hand Tools"},
+            {"index":20.6,"subCategory":"Investment Projects"},
+            {"index":20.7,"subCategory":"Factories Equipments"},
+            {"index":20.8,"subCategory":"Cleaning Supplies & Equipment"},
+            {"index":20.9,"subCategory":"Sewing Supplies & Equipment"},
+            {"index":20.10,"subCategory":"Others"},
+        ]
+    },
+  
+]
+
+module.exports = list
