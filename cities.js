315025049a87ba85495bf5f9a27faa48873be4ea
eihab-dev <108094128+eihab-dev@users.noreply.github.com>
2023-04-16 17:13:45 +0000

    Create cities.js

---
+++ cities.js
@@ -0,0 +1,284 @@
+const list=[
+    {"index":1,"country":"saudi arabia",
+        "cities":[
+            {"index":1.1,"city":"Al-Riyadh"},
+            {"index":1.2,"city":"Makkah"},
+            {"index":1.3,"city":"Al-Madinah"},
+            {"index":1.4,"city":"Al-Qaseem"},
+            {"index":1.6,"city":"Eastern Region"},
+            {"index":1.7,"city":"Aseer"},
+            {"index":1.8,"city":"Tabouk"},
+            {"index":1.9,"city":"Hayel"},
+            {"index":1.10,"city":"Northern Border"},
+            {"index":1.11,"city":"Jazan"},
+            {"index":1.12,"city":"Najran"},
+            {"index":1.13,"city":"Al-Baha"},
+            {"index":1.14,"city":"Al-Jouf"},
+
+    ]},
+    {"index":2,"country":"united arab emirates",
+        "cities":[
+            {"index":2.1,"city":"abu dhabi"},
+            {"index":2.2,"city":"dubai"},
+            {"index":2.3,"city":"sharjah"},
+            {"index":2.4,"city":"fujairah"},
+            {"index":2.5,"city":"ajman"},
+            {"index":2.6,"city":"um al khaimah"}
+    ]},
+    {"index":3,"country":"bahrain",
+        "cities":[
+            {"index":3.1,"city":"Manama"},
+            {"index":3.2,"city":"Riffa"},
+            {"index":3.3,"city":"Muharraq"},
+            {"index":3.4,"city":"Hamad Town"},
+            {"index":3.5,"city":"A'ali"},
+            {"index":3.6,"city":"Isa Town"},
+            {"index":3.7,"city":"Sitra "},
+            {"index":3.8,"city":"Budaiya "},
+            {"index":3.9,"city":"Jidhafs "},
+            {"index":3.10,"city":"Al-Malikiyah "},
+            {"index":3.11,"city":"Jid Ali"},
+            {"index":3.12,"city":"Sanabis"},
+            {"index":3.13,"city":"Tubli"},
+            {"index":3.14,"city":"Durrat Al Bahrain"},
+            {"index":3.15,"city":"Gudaibiya"},
+            {"index":3.16,"city":"Salmabad"},
+            {"index":3.17,"city":"Jurdab"},
+            {"index":3.18,"city":"Diyar Al Muharraq"},
+            {"index":3.19,"city":"Amwaj Islands"},
+            {"index":3.20,"city":"Al Hidd"},
+            {"index":3.21,"city":"Arad"},
+            {"index":3.22,"city":"Busaiteen"},
+            {"index":3.23,"city":"Samaheej"},
+            {"index":3.24,"city":"Al Dair"},
+            {"index":3.25,"city":"Zinj"},
+    ]},
+    {"index":4,"country":"kuwait",
+        "cities":[
+
+{"index":4.1,"city":"Kuwait City"},
+{"index":4.2,"city":"Al Jahrā’"},
+{"index":4.3,"city":"Abū Ḩulayfah"},
+{"index":4.4,"city":"Al Aḩmadī"},
+{"index":4.5,"city":"Al Wafrah"},
+{"index":4.6,"city":"Al Bida‘"},
+{"index":4.7,"city":"Al Farwānīyah"},
+{"index":4.8,"city":"Al ‘Udaylīyah"},
+{"index":4.9,"city":"Abraq Khayţān"},
+{"index":4.10,"city":"Al ‘Abbāsīyah"},
+{"index":4.11,"city":"Ash Shadādīyah"},
+{"index":4.12,"city":"Al Quşūr"},
+{"index":4.13,"city":"An Nijfah"},
+{"index":4.14,"city":"Mishrif"},
+{"index":4.15,"city":"Al Massīlah"},
+{"index":4.16,"city":"Al Funayţīs"},
+{"index":4.17,"city":"An Nuqayyāt"},
+{"index":4.18,"city":"Duwaydāt"},
+{"index":4.19,"city":"Al Wafrah"},
+{"index":4.20,"city":"Aḑ Ḑibā‘īyah"},
+{"index":4.22,"city":"Al Maqwa‘"},
+{"index":4.23,"city":"Ar Riqqah"},
+{"index":4.24,"city":"Aş Şabāḩīyah"},
+{"index":4.25,"city":"Al Qulay‘ah"},
+{"index":4.26,"city":"Aţ Ţawīl"},
+{"index":4.27,"city":"Manīfah"},
+{"index":4.28,"city":"Al Manqaf"},
+{"index":4.29,"city":"Al Qurayn"},
+{"index":4.30,"city":"Mubārak al Kabīr"},
+{"index":4.31,"city":"Al ‘Uqaylah"},
+{"index":4.32,"city":"Şubayḩīyah"},
+{"index":4.33,"city":"Ash Shu‘aybah"},
+{"index":4.34,"city":"Al Khīrān"},
+{"index":4.35,"city":"Al Mahbūlah"},
+{"index":4.36,"city":"Az Zawr"}
+    ]},
+    {"index":5,"country":"qatar",
+        "cities":[
+            {"index":1.1,"city":"Doha"},
+            {"index":1.1,"city":"Abu az Zuluf"},
+            {"index":1.1,"city":"Abu Thaylah"},
+            {"index":1.1,"city":"Ad Dawhah al Jadidah"},
+            {"index":1.1,"city":"Al `Arish"},
+            {"index":1.1,"city":"Al Bida` ash Sharqiyah"},
+            {"index":1.1,"city":"Al Ghanim"},
+            {"index":1.1,"city":"Al Ghuwariyah"},
+            {"index":1.1,"city":"Al Hilal al Gharbiyah"},
+            {"index":1.1,"city":"Al Hilal ash Sharqiyah"},
+            {"index":1.1,"city":"Al Hitmi"},
+            {"index":1.1,"city":"Al Jasrah"},
+            {"index":1.1,"city":"Al Jumaliyah"},
+            {"index":1.1,"city":"Al Ka`biyah"},
+            {"index":1.1,"city":"Al Khalifat"},
+            {"index":1.1,"city":"Al Khor"},
+            {"index":1.1,"city":"Al Khawr"},
+            {"index":1.1,"city":"Al Khuwayr"},
+            {"index":1.1,"city":"Al Mafjar"},
+            {"index":1.1,"city":"Al Qa`abiyah"},
+            {"index":1.1,"city":"Al Wakrah, second city"},
+            {"index":1.1,"city":"Al `Adhbah"},
+            {"index":1.1,"city":"An Najmah"},
+            {"index":1.1,"city":"Ar Rakiyat"},
+            {"index":1.1,"city":"Al Rayyan"},
+            {"index":1.1,"city":"Ar Ru'ays"},
+            {"index":1.1,"city":"As Salatah"},
+            {"index":1.1,"city":"As Salatah al Jadidah"},
+            {"index":1.1,"city":"As Sani`"},
+            {"index":1.1,"city":"As Sawq"},
+            {"index":1.1,"city":"Ath Thaqab"},
+            {"index":1.1,"city":"Blaré"},
+            {"index":1.1,"city":"Dukhan"},
+            {"index":1.1,"city":"Ras Laffan Industrial City"},
+            {"index":1.1,"city":"Umm Bab"},
+            {"index":1.1,"city":"Umm Sa'id"},
+            {"index":1.1,"city":"Umm Salal Ali"},
+            {"index":1.1,"city":"Umm Salal Mohammed"},
+    ]},
+    {"index":6,"country":"oman",
+        "cities":[
+            {"index":1.1,"city":"Adam"},
+            {"index":1.1,"city":"As Sib"},
+            {"index":1.1,"city":"Al Ashkharah"},
+            {"index":1.1,"city":"Al Buraimi"},
+            {"index":1.1,"city":"Al Hamra"},
+            {"index":1.1,"city":"Al Jazer"},
+            {"index":1.1,"city":"Al Madina A'Zarqa"},
+            {"index":1.1,"city":"Al Suwaiq"},
+            {"index":1.1,"city":"Bahla"},
+            {"index":1.1,"city":"Barka"},
+            {"index":1.1,"city":"Bidbid"},
+            {"index":1.1,"city":"Bidiya"},
+            {"index":1.1,"city":"Duqm"},
+            {"index":1.1,"city":"Haima"},
+            {"index":1.1,"city":"Ibra"},
+            {"index":1.1,"city":"Ibri"},
+            {"index":1.1,"city":"Izki"},
+            {"index":1.1,"city":"Jabrin"},
+            {"index":1.1,"city":"Jalan Bani Bu Hassan"},
+            {"index":1.1,"city":"Khasab"},
+            {"index":1.1,"city":"Mahooth"},
+            {"index":1.1,"city":"Manah"},
+            {"index":1.1,"city":"Masirah"},
+            {"index":1.1,"city":"Matrah"},
+            {"index":1.1,"city":"Mudhaybi"},
+            {"index":1.1,"city":"Mudhaireb"},
+            {"index":1.1,"city":"Muscat"},
+            {"index":1.1,"city":"Nizwa"},
+            {"index":1.1,"city":"Quriyat"},
+            {"index":1.1,"city":"Raysut"},
+            {"index":1.1,"city":"Rustaq"},
+            {"index":1.1,"city":"Ruwi"},
+            {"index":1.1,"city":"Saham"},
+            {"index":1.1,"city":"Shinas"},
+            {"index":1.1,"city":"Saiq"},
+            {"index":1.1,"city":"Salalah"},
+            {"index":1.1,"city":"Samail"},
+            {"index":1.1,"city":"Sohar"},
+            {"index":1.1,"city":"Sur"},
+            {"index":1.1,"city":"Tan`am"},
+            {"index":1.1,"city":"Thumrait"},
+    ]},
+    {"index":7,"country":"jordan",
+        "cities":[
+            {"index":1.1,"city":"Amman"},
+            {"index":1.1,"city":"Zarqa"},
+            {"index":1.1,"city":"Irbid"},
+            {"index":1.1,"city":"Russeifa"},
+            {"index":1.1,"city":"Sahab"},
+            {"index":1.1,"city":"Ar Ramtha"},
+            {"index":1.1,"city":"Aqaba"},
+            {"index":1.1,"city":"Mafraq"},
+            {"index":1.1,"city":"Madaba"},
+            {"index":1.1,"city":"As-Salt"},
+            {"index":1.1,"city":"Al-Jizah"},
+            {"index":1.1,"city":"Ain Al-Basha"},
+            {"index":1.1,"city":"Aydoun"},
+            {"index":1.1,"city":"Ad-Dhlail"},
+            {"index":1.1,"city":"Jerash"},
+            {"index":1.1,"city":"As-Sarih"},
+            {"index":1.1,"city":"Al-Hashimiyah"},
+            {"index":1.1,"city":"Ma'an"},
+            {"index":1.1,"city":"Beit Ras"},
+            {"index":1.1,"city":"Al Husn"},
+            {"index":1.1,"city":"At-Turrah"},
+            {"index":1.1,"city":"Naour"},
+            {"index":1.1,"city":"Karak"},
+            {"index":1.1,"city":"Kufranjah"},
+            {"index":1.1,"city":"Der Abi Saeed"},
+            {"index":1.1,"city":"No'ayymeh"},
+            {"index":1.1,"city":"Ash-Shajarah"},
+            {"index":1.1,"city":"Tafilah"},
+            {"index":1.1,"city":"Mu'tah"},
+            {"index":1.1,"city":"Ghor es-Safi"},
+            {"index":1.1,"city":"Anjara"},
+            {"index":1.1,"city":"Al Mashar'e"},
+            {"index":1.1,"city":"Huwwarah"},
+            {"index":1.1,"city":"Kafr Yubah"},
+            {"index":1.1,"city":"Kuraymeh"},
+            {"index":1.1,"city":"At-Taybeh"},
+            {"index":1.1,"city":"Al-Mazar Ash-Shamali"},
+            {"index":1.1,"city":"As-Sukhneh"},
+            {"index":1.1,"city":"Al-Khalidiyah Al-Jadeedah 	"},
+            {"index":1.1,"city":"Bushra"},
+            {"index":1.1,"city":"Um Al-Sumaq Al-Janoubi"},
+            {"index":1.1,"city":"Judayteh"},
+            {"index":1.1,"city":"Fuheis"},
+            {"index":1.1,"city":"Al-Mazar Al-Janoubi"},
+            {"index":1.1,"city":"Shuna Ash-Shamaliyeh"},
+            {"index":1.1,"city":"Kafr Al-Ma’ "},
+            {"index":1.1,"city":"Mahis"},
+        
+    ]},
+    
+]
+
+module.exports = list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
